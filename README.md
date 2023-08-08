918f830d2427cf81fdcb1325032de151a60bd82d
windlil <spacesouls@163.com>
2023-08-08 15:28:20 +0800

    chore: modify

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # vue-armor
 🛸Vue macros that provide additional attributes to vue elements.🛸为Vue标签提供更多属性
+
+## Installation
+```bash
+pnpm i unplugin-vue-armor -D
+```
--- src/core/unplugin.ts
+++ src/core/unplugin.ts
@@ -1,8 +1,15 @@
 import { createUnplugin } from 'unplugin'
 import { createFilter } from '@rollup/pluginutils'
 
+const enum TransformType {
+  STYLE = 0,
+  NAME = 1,
+}
+
 const styleExistReg = new RegExp(/<script(?:[\s\S]*?)style=(?:"|')(.*?)(?:"|')(?:[\s\S]*?)>/)
+const nameExistReg = new RegExp(/<script(?:[\s\S]*?)name=(?:"|')(.*?)(?:"|')(?:[\s\S]*?)>/)
 const stylePathReg = new RegExp(/style=((?:'|")\S*(?:'|"))/)
+const nameReg = new RegExp(/name=((?:'|")\S*(?:'|"))/)
 
 export default createUnplugin(() => {
   const filter = createFilter(
@@ -16,20 +23,34 @@ export default createUnplugin(() => {
       return filter(id)
     },
     transform(code: string) {
-      const scriptElString = code.match(styleExistReg)
-      if (scriptElString)
-        code = transformCode(code, scriptElString[0])
+      const styleElString = code.match(styleExistReg)
+      const nameElstring = code.match(nameExistReg)
+      if (styleElString)
+        code = transformCode(code, styleElString[0], TransformType.STYLE)
+      if (nameElstring)
+        code = transformCode(code, nameElstring[0], TransformType.NAME)
       return code
     },
   }
 })
 
-function transformCode(code: string, scriptElString: string): string {
-  const stylePath = stylePathReg.exec(scriptElString)
+function transformCode(code: string, scriptElString: string, type: number): string {
+  const initScriptString = scriptElString
   let styleString = ''
-  if (stylePath) {
-    code = code.replace(stylePath[0], '')
-    styleString = `\n<style scoped src=${stylePath[1]}></style>`
+  let stylePath
+  console.log(scriptElString)
+  switch (type) {
+    case 0:
+      stylePath = stylePathReg.exec(scriptElString)
+      if (stylePath) {
+        scriptElString = scriptElString.replace(stylePath[0], '')
+        code = code.replace(initScriptString, scriptElString)
+        styleString = `\n<style scoped src=${stylePath[1]}></style>`
+      }
+      break
+    case 1:
+      break
   }
+  console.log(type)
   return code + styleString
 }
